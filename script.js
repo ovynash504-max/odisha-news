22fe77a3a527d016e9b255239e9ce180c71871f4
ovynash504-max <barikabinash504@gmail.com>
2026-04-22 16:07:50 +0530

    Update script.js
    Daily Odisha Breaking News Website | Jobs, Results, Sports, Tech Updates | Auto News System

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const apiKey = "2e1a0bb9e3e8661701ea292d24bd11ce";
 
 async function getNews() {
-  let url = `https://gnews.io/api/v4/search?q=odisha&lang=en&max=10&token=${2e1a0bb9e3e8661701ea292d24bd11ce}`;
+  let url = `https://gnews.io/api/v4/search?q=odisha&lang=en&max=10&token=2e1a0bb9e3e8661701ea292d24bd11ce`;
   try {
     let res = await fetch(url);
     let data = await res.json();
